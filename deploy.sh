08f1ce2dd82f1dcba08c7ce09db48e73bda53310
Antonio Maschio <tonibin@tonibin.home-life.hub>
2026-06-23 17:10:39 +0200

    Site update:

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,8 @@
-# Inizializza il repository locale (se non lo hai già fatto)
-git init
-
 # Aggiungi tutti i tuoi file
 git add .
 
 # Crea il primo commit
-git commit -m "Primo caricamento del sito"
-
-# Specifica il branch principale (deve chiamarsi 'main')
-git branch -M main
-
-# Collega il tuo computer al repository online
-git remote add origin https://github.com/tonibin/tonibin.github.io.git
+git commit -m "Site update: ${date}"
 
 # Carica tutto su GitHub
 git push -u origin main
